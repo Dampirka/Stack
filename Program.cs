673b1cb15b88724b18f022e74f92bb96b916cbc3
Dampirka <73588152+Dampirka@users.noreply.github.com>
2020-11-04 18:52:50 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using stack.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sstack = new sstack <int>();
+
+            sstack.Push(47);
+            sstack.Push(78);
+            sstack.Push(58);
+
+            var item = sstack.Pop();
+            var item2 = sstack.Peek();
+
+            Console.WriteLine(item);
+            Console.WriteLine(item2);
+            Console.ReadLine();
+        }
+    }
+}
